863f6feda10ef8538a80aa947f7c032ea8169b6a
Ornã Keivison Fernandes Câmara <avelar.e@escolar.ifrn.edu.br>
2023-09-27 15:58:17 -0300

    Inicio de implementação do sensor sonar.

---
+++ RunScibot/SensorDesviar.h
@@ -0,0 +1,20 @@
+#ifndef SENSORDESVIAR_H
+#define SENSORDESVIAR_H
+#include <robo_hardware2.h>
+
+ class SensorDesviar{
+
+  private:
+  
+    float SensorDesviar;
+
+  private:
+
+     inline void Desviar::atualizarSensorDesviar(){ SensorDesviar = robo.lerSensorSonarFrontal();}
+
+     inline float getSensorDesviar() {return (SensorDesviar);}
+
+
+ };
+
+#endif
